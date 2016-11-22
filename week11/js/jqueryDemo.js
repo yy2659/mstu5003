3606dd99f305c7a811a598c27ed6aa02f44c0bcc
jmk2142 <jmk2142@tc.columbia.edu>
2016-11-22 13:36:02 -0500

    Created week11 demonstration

---
+++ week11/js/jqueryDemo.js
@@ -0,0 +1,15 @@
+// Selecting DOM - NATIVE
+document.querySelector('#pie-list');
+
+// vs. JQUERY
+
+$('#pie-list');
+jQuery('#pie-list'); // $ is the name of a function, $ = jQuery;
+
+// But what is returned is different...
+
+
+// Manipulating DOM
+
+
+// Traversing DOM
