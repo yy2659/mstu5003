87c80dc1641ddec322765ec4979c5046e149d33a
Jin Kuwata <jmk2142@tc.columbia.edu>
2016-12-06 16:10:32 -0500

    Updated

--- week13/main.js
+++ week13/main.js
@@ -4,3 +4,11 @@ var currentUser = {
 	last: "Kuwata",
 	id: "a1"
 };
+
+var userMsg = document.querySelector('#userText').value;
+
+// Program the component to take text data... and store it / when the button is pushed.
+
+document.querySelector('#postBtn').addEventListener('click', function(){
+	alert('thing in textarea gets printed here');
+});
